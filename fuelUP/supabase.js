b75109dfe4a89d5979561442d128531a82332c6f
Samiksha More <156448739+Samiksha4102@users.noreply.github.com>
2026-01-02 16:31:26 +0530

    Delete fuelUP directory

--- fuelUP/supabase.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://abcd1234.supabase.co';
-const supabaseAnonKey = 'YOUR_ANON_KEY';
-
-export const supabase = createClient(
-    supabaseUrl,
-    supabaseAnonKey
-);
